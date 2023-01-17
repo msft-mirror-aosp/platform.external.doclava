6c186c0d3aafdae33d84528e58b8b0a305212be4
Nikita Iashchenko <nikitai@google.com>
2023-01-17 12:14:01 +0000

    doclava17: [no tests] Add ParameterizedTypeImpl#typeArguments()
    Current progress is 140/147 methods implemented (95.23%).
    
    Bug: 260694901
    Test: ./gradlew doclava17:test
    Change-Id: Id964d68c9ced390b90e7e6e87f225ad9e359aeb3

--- doclava17/src/main/java/com/google/doclava/javadoc/ParameterizedTypeImpl.java
+++ doclava17/src/main/java/com/google/doclava/javadoc/ParameterizedTypeImpl.java
@@ -30,12 +30,17 @@ import com.google.doclava.annotation.Used;
 import com.sun.javadoc.ParameterizedType;
 import com.sun.javadoc.Type;
 import javax.lang.model.type.DeclaredType;
-import javax.lang.model.type.TypeMirror;
 
 class ParameterizedTypeImpl extends TypeImpl implements ParameterizedType {
 
-    protected ParameterizedTypeImpl(TypeMirror typeMirror, Context context) {
-        super(typeMirror, context);
+    private final DeclaredType declaredType;
+
+    // Cached fields
+    private Type[] typeArguments;
+
+    protected ParameterizedTypeImpl(DeclaredType declaredType, Context context) {
+        super(declaredType, context);
+        this.declaredType = declaredType;
     }
 
     static ParameterizedTypeImpl create(DeclaredType declaredType, Context context) {
@@ -50,9 +55,15 @@ class ParameterizedTypeImpl extends TypeImpl implements ParameterizedType {
     }
 
     @Override
-    @Used
+    @Used(implemented = true)
     public Type[] typeArguments() {
-        throw new UnsupportedOperationException("not yet implemented");
+        if (typeArguments == null) {
+            typeArguments = declaredType.getTypeArguments()
+                    .stream()
+                    .map(typeMirror -> TypeImpl.create(typeMirror, context))
+                    .toArray(Type[]::new);
+        }
+        return typeArguments;
     }
 
     @Override
